39eb31b8e43f509e506d8a6b89461d966afc402f
madhur-taneja <tanejamadhur96@gmail.com>
2020-04-22 00:01:57 +0530

    Assignment 1
    Adding comments section functionality

--- src/components/DishdetailComponent/DishdetailComponent.js
+++ src/components/DishdetailComponent/DishdetailComponent.js
@@ -7,26 +7,24 @@ class DishdetailComponent extends Component {
 
   constructor(props) {
     super(props);
-
+  
     this.state = {
       
     };
   }
 
-  render() {
-    const dish = this.props.dish;
+  renderSelectedDish(dish) {
+    //console.log("Selected dish in Dishdetail " + JSON.stringify(dish));
     if (dish != null)
       return(
-        <div className="row bg-color">
-          <div className="col-12 col-md-5 m-1">
-            <Card>
+        <div className="col-12 col-md-5 m-1">
+          <Card key={dish.id}>
               <CardImg top src={dish.image} alt={dish.name} />
               <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
               </CardBody>
             </Card>
-          </div>
         </div>
       );
     else
@@ -34,6 +32,49 @@ class DishdetailComponent extends Component {
           <div></div>
       );
   }
+
+  renderComments(comments) {
+    if(comments == null) {
+      return (
+        <div></div>
+      );
+    }
+    const commt = comments.map((comment) => {
+      return (
+          <li key={comment.id}>
+            <p>{comment.comment}</p>
+            <p>-- {comment.author}, {comment.date}</p>
+            <p>- - - - - - - - - - - - - -</p>
+          </li>
+      );  
+    });
+    return (
+      <div className='col-12 col-md-5 m-1'>
+          <h4> Comments </h4>
+          <ul className='list-unstyled'>
+              {commt}
+          </ul>
+      </div>
+    )
   }
+
+  render() {
+    const dish = this.props.selectedDish;
+    if (dish == null) {
+      return (<div></div>)
+    }
+
+    else {
+      return (
+        <div className="row bg-color">
+          {this.renderSelectedDish(dish)}
+          {this.renderComments(dish.comments)}
+        </div>
+      )
+    }
+  }
+
+
+}
 
 export default DishdetailComponent;
