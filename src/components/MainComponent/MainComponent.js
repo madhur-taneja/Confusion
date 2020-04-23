1ff6f0d39d0c21f934651a7e6d61f9be396aca6f
madhur-taneja <tanejamadhur96@gmail.com>
2020-04-23 07:54:16 +0530

    Adding Header and Footer

--- src/components/MainComponent/MainComponent.js
+++ src/components/MainComponent/MainComponent.js
@@ -1,34 +1,32 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import MenuComponent from '../MenuComponent/MenuComponent';
 import DishdetailComponent from '../DishdetailComponent/DishdetailComponent';
 import { DISHES } from '../../shared/dishes';
+import HeaderComponent from '../HeaderComponent/HeaderComponent';
+import FooterComponent from '../FooterComponent/FooterComponent';
 
 class MainComponent extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-        dishes: DISHES,
-        selectedDish: null
+      dishes: DISHES,
+      selectedDish: null
     };
   }
 
   onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
+    this.setState({ selectedDish: dishId });
   }
 
   render() {
     //console.log("Selected dish in Main " + JSON.stringify(dish));
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <HeaderComponent />
         <MenuComponent dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
         <DishdetailComponent selectedDish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <FooterComponent />
       </div>
     );
   }
