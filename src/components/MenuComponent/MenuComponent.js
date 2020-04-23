46e417f53712344a64301fe69cdcba8f886cd6a6
madhur-taneja <tanejamadhur96@gmail.com>
2020-04-23 08:16:25 +0530

    Updating the UI

--- src/components/MenuComponent/MenuComponent.js
+++ src/components/MenuComponent/MenuComponent.js
@@ -18,7 +18,7 @@ const MenuComponent = (props) => {
 
   const menu = props.dishes.map((dish) => {
     return (
-      <div className="col-12 col-md-5 m-1" key={dish.id}>
+      <div className="col-12 col-md-6 my-1" key={dish.id}>
         <RenderMenuItem dish={dish} onClick={props.onClick} />
       </div>
     );
