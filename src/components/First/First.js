3924d715fb91e2e69db3c5f5699c1889189c58af
madhur-taneja <tanejamadhur96@gmail.com>
2020-04-23 09:05:46 +0530

    Adding React Router

--- src/components/First/First.js
+++ src/components/First/First.js
@@ -4,7 +4,21 @@ import './First.css';
 
 const First = () => (
   <div className="First">
-    First Component
+    <p>First Component</p>
+    <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+          </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+          </a>
+  </header>
   </div>
 );
 
